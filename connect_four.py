9bdac5b1b3acf60743e156411738f2ff7574ecdb
Sivaraj <K1518127@students.katyisd.org>
2025-09-05 08:09:34 -0500

    welp

--- connect_four.py
+++ connect_four.py
@@ -14,19 +14,27 @@ startx = 20
 starty = 100
 endx = startx+100
 endy = starty+100
+count=0
+
 for i in range(6):
     for j in range(7):
-        canvas.create_oval(startx,starty,endx,endy,outline="black", fill="white", width=3)
+        count = canvas.create_oval(startx,starty,endx,endy,outline="black", fill="white", width=3)
         startx+=100+15
         endx+=100+15
+        count+=1
     startx =20
     endx  = 20+100
     starty+=100+15
     endy+=100+15
-
     
+def on_button_click():
+    pass
+    
+button = tk.Button(root, text="Click Me", command=on_button_click)
+#canvas.create_window()
+button.place(x=150, y=100, width=100, height=50)
 
-
+#canvas.itemconfigure("1", fill="red")
 turn = True # red starts
 count = 0
 
